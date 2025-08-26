1cb466336771cc32fc920136d97ca52ab69e9661
Lemoine <llemoine@una.edu>
2025-08-26 13:45:42 -0500

    new things

--- app.mjs
+++ app.mjs
@@ -1,14 +1,24 @@
 import express from 'express'
-
+// import path from 'path';
 const app = express()
 
 const PORT = process.env.PORT || 3000;
+
+const path = require('path');
+
+app.use(express.static(__dirname + 'public'));
+
 app.get('/', (req, res) => {
-  res.send('Hello Express from Render 😍😍😍')
+  res.send('Hello Express from Render 😍😍😍. <a href="laura">laura</a>')
 })
 
 // endpoints...middlewares...apis?
 // send an html file
+
+app.get('/laura', (req, res) => {
+  res.send('laura. <a href="/">home</a>')
+  res.sendFile('laura.html');
+})
 
 app.listen(3000)
 //Push #1 testing
