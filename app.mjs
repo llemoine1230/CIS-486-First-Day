72c0b66f97f79b4e6d40ffda0eae6569186eefbe
Lemoine <llemoine@una.edu>
2025-09-02 13:39:12 -0500

    9/2

--- app.mjs
+++ app.mjs
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 3000;
 //const path = require('path');
 
 app.use(express.static(join(__dirname, 'public')));
-
+app.use(express.json());
 app.get('/', (req, res) => {
   res.send('Hello Express from Render 😍😍😍. <a href="laura">laura</a>')
 })
@@ -25,6 +25,24 @@ app.get('/api/laura', (req, res) => {
   const myVar = 'Hello from server!';
   res.json({ myVar });
 })
+
+app.get('/api/query', (req, res) => {
+  console.log("client request with query param:", req.query.name);
+  const name = req.query.name;
+  res.json({"message": `Hi, ${name}. How are you?`});
+});
+
+app.get('/api/url/:id', (req, res) => {
+  console.log("client request with URL param:", req.params.id);
+  //const name = req.query.name;
+  //res.json({"message": `Hi, ${name}. How are you?`});
+});
+
+app.post('/api/body', (req, res) => {
+  console.log("client request with POST body:", req.body);
+  // const name = req.body.name;
+  // res.json({"message": `Hi, ${name}. How are you?`});
+});
 app.listen(3000)
 //Push #1 testing
 // TODO: refactor to use the environment port.
